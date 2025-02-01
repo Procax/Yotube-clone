32db271fe66089948564581fa55314dd58985091
Procax <babablack626@gmail.com>
2025-02-02 00:06:14 +0530

    card fix

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,10 +6,10 @@ const VideoCard = ({info}) => {
     const {channelTitle, title, thumbnails} = snippet
 
   return (
-    <div className='p-2 m-2 w-72 shadow-lg'>
+    <div className='p-2 m-2 w-72 h-96 shadow-lg'>
       <img className='rounded-lg' src={thumbnails.high.url}/>
       <ul>
-        <li className='font-bold py-2'>{title}</li>
+        <li className='font-bold py-2 break-words'>{title}</li>
         <li>{channelTitle}</li>
         <li>{statistics.viewCount}</li>
       </ul>
